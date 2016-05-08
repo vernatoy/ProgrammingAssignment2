70d93a4a63c80a4a41ff36d2d5eaaa3be845aba8
vernatoy <verna.toy@sympatico.ca>
2016-05-08 11:03:06 -0400

    Update cachematrix.R
    Updated as required to create an inverse matrix as per assignment directive.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment #2: Lexical Scoping
 
-## Write a short comment describing this function
+## Create a special "matrix" object that can cache its own inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+  makeCacheMatrix <- function(x = matrix()) {
+    
+    mat_inv<- NULL
+    set <- function(y) {
+        x <<- y
+        mat_inv<<- NULL
+        }
+    get <- function() x
+    setInverse <- function(inverse) mat_inv<<- inverse
+    getInverse <- function() mat_inv
+    list(set = set,get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+  }
 
-}
+## Compute the inverse of the matrix created by makeCacheMatrix.  
+## If the inverse has already been calculated, it will retrieve the matrix(makeCacheMatrix) 
+## If not, it will calculate the inverse of the matrix 
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  cacheSolve <- function(x, ...) {
+    mat_inv<- x$getInverse()
+    ## First check to see if the matrix is in the cache already
+    
+    if (!is.null(mat_inv)) {
+        message("getting data from cache")
+        return(mat_inv)
+    }
+    ## If the matrix does not exist, create a new matrix and cache it. 
+    ## Print the inverse matrix
+    
+      message("creating new inverse matrix")
+      mat <- x$get()
+      mat_inv<- solve(mat, ...)
+      x$setInverse(mat_inv)
+      mat_inv
+  }
